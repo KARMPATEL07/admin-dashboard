f06ad850b340fbfe63c1abecec1922b61c8bdb80
KARMPATEL07 <karmpatel432.pf@gmail.com>
2024-02-01 16:48:20 +0530

    dashbaord-revenue chart completed

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,7 +3,8 @@ import AdminSidebar from "../components/AdminSidebar";
 import { BsSearch } from "react-icons/bs";
 import { FaRegBell } from "react-icons/fa";
 import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
-import data from"../assets/data.json"
+import data from "../assets/data.json"
+import { BarChart } from "../components/Charts";
 
 const userImg =
   "https://media.licdn.com/dms/image/D4D35AQE5osco-SJJ2Q/profile-framedphoto-shrink_400_400/0/1698775377644?e=1707332400&v=beta&t=R5wuOO67c6TyGvTZgPXShXU_M8v18-OP_09hlTs3CSk";
@@ -51,9 +52,13 @@ const dashboard = () => {
         </section>
 
         {/* ..........Graphical-Info................. */}
+
         <section className="graph-container">
           <div className="revenue-chart">
             <h2>Revenue & Transactions</h2>
+
+            <BarChart data_1={[300, 144, 433, 655, 237, 755, 190]} data_2={[200, 444, 343, 556, 778, 455, 990]} title_1="Revenue" title_2="Transactions" bgColor_1="rgb(0,115,255)" bgColor_2="rgba(53,162,235,0.8" />
+
           </div>
           <div className="dashboard-cat">
             <h2>Inventory</h2>
@@ -63,18 +68,19 @@ const dashboard = () => {
                   key={index}
                   title={item.title}
                   value={item.value}
-                  color={`hsl(${item.value*60}, ${item.value}%, 50%)`}
+                  color={`hsl(${item.value * 60}, ${item.value}%, 50%)`}
                 />
               ))}
             </div>
           </div>
         </section>
+
       </main>
     </div>
   );
 };
 
-{       /* ..........Widgets................. */     }
+{       /* ..........Widgets................. */ }
 interface WidgetProps {
   title: string;
   value: number;
@@ -108,36 +114,35 @@ const WidgetItem = ({
     <div
       className="widget-circle"
       style={{
-        background: `conic-gradient(${color} ${
-          (Math.abs(percentage) / 100) * 360
-        }deg, rgb(255,255,255) 0)`,
+        background: `conic-gradient(${color} ${(Math.abs(percentage) / 100) * 360
+          }deg, rgb(255,255,255) 0)`,
       }}
     >
       <span style={{ color }}>{percentage}%</span>
     </div>
   </article>
 );
-  // Graphial-info...............
+// Graphial-info...............
 
-  interface CatagoryItemProps {
-    title: string;
-    value: number;
-    color: string;
-  }
+interface CatagoryItemProps {
+  title: string;
+  value: number;
+  color: string;
+}
 
-  const CatagoryItem = ({ title, value, color }: CatagoryItemProps) => (
-    <div className="dashboard-cat-item">
-      <h5>{title}</h5>
-      <div>
-        <div style={{
-          background: color,
-          width: `${value}%`,
-        }}></div>
-      </div>
-      <span>
-        {value}%
-      </span>
+const CatagoryItem = ({ title, value, color }: CatagoryItemProps) => (
+  <div className="dashboard-cat-item">
+    <h5>{title}</h5>
+    <div>
+      <div style={{
+        background: color,
+        width: `${value}%`,
+      }}></div>
     </div>
+    <span>
+      {value}%
+    </span>
+  </div>
 
 );
 
